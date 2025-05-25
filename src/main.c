b193313da469a8a025a597e94ab22419053d2487
vitoralvesp <pereira.alves.vitor@outlook.com>
2025-05-25 14:23:08 -0300

    trabalhando na impressão da notação pósfixa

--- src/main.c
+++ src/main.c
@@ -19,23 +19,43 @@
  * Referências:
  * [1] https://www.geeksforgeeks.org/fgets-function-in-c/
  * [2] https://github.com/zewuchen/asdr/blob/master/asdr.c
- * 
+ * [3] https://www.geeksforgeeks.org/implement-stack-using-array/
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 // Gramática
-int expr(char **word);
-int expr_(char **word);
-int term(char **word);
-int term_(char **word);
-int factor(char **word);
+int expr(char **word, char** posfix);
+int expr_(char **word, char** posfix);
+int term(char **word, char** posfix);
+int term_(char **word, char** posfix);
+int factor(char **word, char** posfix);
+
+// Estrutura da Pilha
+struct Stack {
+    int top;
+    int capacity;
+    int *array;
+};
+
+struct Stack *stack;
+
+struct Stack *createStack(int capacity);
+void deleteStack(struct Stack *stack);
+int isFull(struct Stack *stack);
+int isEmpty(struct Stack *stack);
+int push(struct Stack *stack, int x);
+int pop(struct Stack *stack);
+int peek(struct Stack *stack);
 
 int main() {
 
+    char *posfix = (char *) malloc(50 * sizeof(char));
     char *word = (char *) malloc(50 * sizeof(char));
     char *aux = word;
+
+    stack = createStack(50);
 
     printf(">> enter a word: ");
     fgets(word, 50, stdin);
@@ -52,13 +72,14 @@ int main() {
     // Impede erro de referência ao utilizar o free
     // word++ afeta a referencia 
     aux = word;
-    expr(&aux);
+    expr(&aux, &posfix);
 
     // A palavra é aceita se chegar ao final \0
-    if (*aux == '\0') printf(">> status: palavra aceita!!");
+    if (*aux == '\0') printf(">> %s => palavra aceita!!", posfix);
     else printf(">> status: palavra não aceita!!");
 
     free(word);
+    // free(posfix);
 
     return 0;
 }
@@ -67,11 +88,11 @@ int main() {
  * @brief Reconhece os fatores da palavra
  * @details Os fatores são 1,2,3,4,5,6,7,8,9,(,)
  */
-int factor(char **word) {
+int factor(char **word, char** posfix) {
     switch(**word) {
         case '(':
             (*word)++;
-            expr(word);
+            expr(word, posfix);
             if (**word == ')') {
                 (*word)++;
                 break;
@@ -85,6 +106,7 @@ int factor(char **word) {
         case '7':
         case '8':
         case '9':
+            *(*posfix)++ = **word;
             (*word)++;
             break;
         default:
@@ -95,33 +117,41 @@ int factor(char **word) {
 /**
  * @brief Reconhece os termos da palavra 
  */
-int term(char **word) {
-    factor(word);
-    term_(word);
+int term(char **word, char** posfix) {
+    factor(word, posfix);
+    term_(word, posfix);
 }
 
 /**
  * @brief Função auxiliar ao 'term' 
  */
-int term_(char **word) {
+int term_(char **word, char** posfix) {
+    int calc = 0;
+    char op;
     switch(**word) {
         case '*':
+            op = **word;
             (*word)++;
-            factor(word);
-            term_(word);
+            factor(word, posfix);
+            *(*posfix)++ = op;
+            term_(word, posfix);
             break;
         case '/':
+            op = **word;
             (*word)++;
-            factor(word);
-            term_(word);
+            factor(word, posfix);
+            *(*posfix)++ = op;
+            term_(word, posfix);
             break;
         case '%':
+            op = **word;
             (*word)++;
-            factor(word);
-            term_(word);
+            factor(word, posfix);
+            *(*posfix)++ = op;
+            term_(word, posfix);
             break;
         default:
-            factor(word);
+            factor(word, posfix);
             break;
     }
 }
@@ -129,29 +159,92 @@ int term_(char **word) {
 /**
  * @brief Reconhece as expressões da palavra
  */
-int expr(char **word) {
-    term(word);
-    expr_(word);
+int expr(char **word, char** posfix) {
+    term(word, posfix);
+    expr_(word, posfix);
     return 0;
 }
 
 /**
  * @brief Função auxiliar à 'expr' 
  */
-int expr_(char **word) {
+int expr_(char **word, char** posfix) {
+    char op;
     switch(**word) {
         case '+':
+            op = **word;
             (*word)++;
-            term(word);
-            expr_(word);
+            term(word, posfix);
+            *(*posfix)++ = op;
+            expr_(word, posfix);
             break;
         case '-':
+            op = **word;
             (*word)++;
-            term(word);
-            expr_(word);
+            term(word, posfix);
+            *(*posfix)++ = op;
+            expr_(word, posfix);
             break;
         default:
-            term(word);
+            term(word, posfix);
             break;
     }
+}
+
+/**
+ *
+ */
+struct Stack *createStack(int capacity) {
+    struct Stack *stack = (struct Stack*) malloc (sizeof(struct Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = (int *) malloc(capacity * sizeof(int));
+    return stack;
+}
+
+/**
+ * 
+ */
+void deleteStack(struct Stack *stack) {
+    free(stack->array);
+    free(stack);
+}
+
+/**
+ * 
+ */
+int isFull(struct Stack *stack) {
+    return stack->top >= stack->capacity-1;
+}
+
+/**
+ * 
+ */
+int isEmpty(struct Stack *stack) {
+    return stack->top < 0;
+}
+
+/**
+ * 
+ */
+int push(struct Stack *stack, int x) {
+    if (isFull(stack)) return 1;
+    stack->array[++stack->top] = x;
+    return 0;
+}
+
+/**
+ * 
+ */
+int pop(struct Stack *stack) {
+    if (isEmpty(stack)) return 1;
+    return stack->array[stack->top--];
+}
+
+/**
+ * 
+ */
+int peek(struct Stack *stack) {
+    if (isEmpty(stack)) return 0;
+    return stack->array[stack->top];
 }
