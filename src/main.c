7f010b429c6d6482b7a8773fd23033d723fbfc18
vitoralvesp <pereira.alves.vitor@outlook.com>
2025-05-29 18:30:55 -0300

    Revisão e Adição da Documentação do Projeto

--- src/main.c
+++ src/main.c
@@ -34,16 +34,22 @@ int factor(char **word, char** posfix);
 
 int main() {
 
-    char *posfix = (char *) malloc(50 * sizeof(char));
-    char *word = (char *) malloc(50 * sizeof(char));
-    char *aux_word = word;
-    char *aux_posfix = posfix;
-    int result = 0;
+    char *word;        // palavra inserida pelo usuario
+    char *posfix;      // expressao posfixa da palavra inserida pelo usuario 
+    char *aux_word;    // ponteiro auxiliar para percorrer cada caractere da palavra
+    char *aux_posfix;  // ponteiro auxiliar para percorrer cada caractere da posfixa
+    int result;        // resultado da expressao inserida pelo usuario
+
+    posfix = (char *) malloc(50 * sizeof(char));
+    word = (char *) malloc(50 * sizeof(char));
+    aux_word = word;
+    aux_posfix = posfix;
+    result = 0;
 
     printf(">> insira uma palavra: ");
     fgets(word, 50, stdin);
 
-    // Adiciona \0 na após o último caractere da palavra
+    // Busca pelo ultimo caractere da palavra e adiciona o '\0' na proxima posicao
     while (*aux_word != '\0') {
         if (*aux_word == '\n') {
             *aux_word = '\0';
@@ -52,14 +58,14 @@ int main() {
         aux_word++;
     }
 
-    // Impede erro de referência ao utilizar o free
-    // word++ afeta a referencia 
+    // 'aux_word' volta a apontar para a primeira posicao de 'word', apos os incrementos realizados na repeticao acima
     aux_word = word;
-    result = expr(&aux_word, &aux_posfix);
 
+    // 'expr' recebe os ponteiros auxiliares para garantir que nao existam erros de apontamento apos as incrementacoes sucessivas que serao realizadas
+    result = expr(&aux_word, &aux_posfix);
     *aux_posfix = '\0';
 
-    // A palavra é aceita se chegar ao final \0
+    // A palavra é aceita se chegar ao final '\0'
     if (*aux_word == '\0') printf(">> resultado: %s => %s=%d OK", word, posfix, result);
     else printf(">> resultado: %s => palavra nao aceita por erro sintatico", word);
 
@@ -72,16 +78,25 @@ int main() {
 /**
  * @brief Reconhece os fatores da palavra
  * @details Os fatores são 1,2,3,4,5,6,7,8,9,(,)
+ * @param word ponteiro de ponteiro para a palavra
+ * @param posfix ponteiro de ponteiro para a expressao posfixa
+ * @return retorna o resultado de uma expressao entre parenteses ou um inteiro de 1 a 9 
  */
 int factor(char **word, char** posfix) {
+    int result;
+    char number;
+
     switch(**word) {
         case '(':
             (*word)++;
-            int result = expr(word, posfix);
+            result = expr(word, posfix);
             if (**word == ')') {
                 (*word)++;
                 return result;
-            } else exit(1);
+            } else {
+                printf(">> resultado: erro sintatico");
+                exit(1);
+            }
         case '1':
         case '2':
         case '3':
@@ -91,15 +106,19 @@ int factor(char **word, char** posfix) {
         case '7':
         case '8':
         case '9':
-            char value = **word;      
-            *(*posfix)++ = value;
+            number = **word;      
+            *(*posfix)++ = number;
             (*word)++;
-            return value - '0';
+            return number - '0';
+        
     }
 }
 
 /**
- * @brief Reconhece os termos da palavra 
+ * @brief Reconhece os termos da palavra
+ * @param word ponteiro de ponteiro para a palavra
+ * @param posfix ponteiro de ponteiro para a expressao posfixa
+ * @return retorna o resultado de um termo
  */
 int term(char **word, char** posfix) {
     int result = factor(word, posfix);
@@ -107,11 +126,16 @@ int term(char **word, char** posfix) {
 }
 
 /**
- * @brief Função auxiliar ao 'term' 
+ * @brief Função auxiliar ao 'term' para obter o resultado de um termo
+ * @param word ponteiro de ponteiro para a palavra
+ * @param posfix ponteiro de ponteiro para a expressao posfixa
+ * @param result resultado de um termo
+ * @return retorna o resultado de um termo
  */
 int term_(char **word, char** posfix, int result) {
     int factor_result;
     char op;
+
     switch(**word) {
         case '*':
             op = **word;
@@ -137,7 +161,10 @@ int term_(char **word, char** posfix, int result) {
 }
 
 /**
- * @brief Reconhece as expressões da palavra
+ * @brief Reconhece as expressoes da palavra
+ * @param word ponteiro de ponteiro para a palavra
+ * @param posfix ponteiro de ponteiro para a expressao posfixa
+ * @return retorna o resultado de uma expressao
  */
 int expr(char **word, char** posfix) {
     int result = term(word, posfix);
@@ -145,7 +172,11 @@ int expr(char **word, char** posfix) {
 }
 
 /**
- * @brief Função auxiliar à 'expr' 
+ * @brief Função auxiliar à 'expr' para obter o resultado de uma expressao
+ * @param word ponteiro de ponteiro para a palavra
+ * @param posfix ponteiro de ponteiro para a expressao posfixa
+ * @param result resultado de uma expressao
+ * @return retorna o resultado de uma expressao
  */
 int expr_(char **word, char** posfix, int result) {
     int term_result;
